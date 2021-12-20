5cde4d80099f20c133e6abd352cac49dbb6b7570
dekapon <lemeindi@gmail.com>
2021-12-20 22:10:02 +0100

    done - not cleaned implemented duration and dir in turnMot()

--- meindi/Core/Src/main.c
+++ meindi/Core/Src/main.c
@@ -22,11 +22,13 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-#include <stdbool.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include <stdbool.h>
+#include <string.h>
+#include <stdbool.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,6 +49,7 @@
 
 /* USER CODE BEGIN PV */
 enum Speed {Slow = 1999, Medium = 299, Fast = 139};
+enum Dir{Right, Left};
 bool isHoming = false;
 bool isEndstopTriggered = false;
 void startTimer(){
@@ -67,32 +70,41 @@ void sendMessage(char *messageToSend){
 	strcpy(message, messageToSend);
 	strcat(message, messageEnding);
 	HAL_UART_Transmit(&huart2, (uint8_t*)message, messageLen, HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart4, (uint8_t*)message, messageLen, HAL_MAX_DELAY);
 	free(message);
 }
-void turnmot(int per){
+void turnmot(int speed, int time, int dir){
 	/*for (int i = 1; i < 2000; ++i)
 	  {
 		HAL_GPIO_TogglePin(motStep_GPIO_Port, motStep_Pin);
 		HAL_Delay(2);
 	  }*/
+	if(dir == 1){
+		HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, 1);
+	}
+	else{
+		HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, 0);
+	}
 	startTimer();
-	TIM2->PSC = per;
-	HAL_Delay(2000);
+	TIM2->PSC = speed;
+	HAL_Delay(time);
 	stopTimer();
 }
 
 void goHome(){
-	HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, 1);
+	HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, Left);
 	startTimer();
 	//HAL_GPIO_WritePin(eV1_GPIO_Port, eV1_Pin, 1);
 	TIM2->PSC = Medium;
 	while(isEndstopTriggered != true){
 	}
 	sendMessage("triggered !");
-	HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, 0);
+	HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, Right);
+	startTimer();
 	TIM2->PSC = Slow;
 	HAL_Delay(500);
 	stopTimer();
+	isEndstopTriggered = false;
 	sendMessage("done !");
 	//HAL_GPIO_WritePin(eV1_GPIO_Port, eV1_Pin, 0);
 }
@@ -139,20 +151,21 @@ void decodeMessage(char key){
 		sendMessage("Electrovalve 2 OFF!");
 		break;
 	case 'b':
-		HAL_GPIO_WritePin(motStep_GPIO_Port, motStep_Pin, 1);
-		sendMessage("Motor step ON!");
+		//HAL_GPIO_WritePin(motStep_GPIO_Port, motStep_Pin, 1);
+		//sendMessage("Motor step ON!");
 		//turnmot(1999); // 1999 = 40 Hz, 199 = 394khz
 		//turnmot(1999); // slow
 		//turnmot(499); // 1kHz
 		//turnmot(899); // 553 Hz
 		//turnmot(299); // 1.66 kHz // moderate speed
 		//turnmot(199); // 2.49 khZ
-		turnmot(Medium); // 10 kHz // fast
+		turnmot(Fast, 2300, Right);
 		//smoothTurn(199);
 		break;
 	case 'n':
-		HAL_GPIO_WritePin(motStep_GPIO_Port, motStep_Pin, 0);
-		sendMessage("Motor step OFF!");
+		//HAL_GPIO_WritePin(motStep_GPIO_Port, motStep_Pin, 0);
+		//sendMessage("Motor step OFF!");
+		turnmot(Fast, 2300, Left);
 		break;
 	case 'c':
 		HAL_GPIO_WritePin(motDir_GPIO_Port, motDir_Pin, 1);
@@ -165,6 +178,9 @@ void decodeMessage(char key){
 	case 'l':
 			sendMessage("Homing !");
 			goHome();
+			break;
+	case 'm':
+			turnmot(Fast, 4600, Left);
 			break;
 
 	}
@@ -213,6 +229,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM2_Init();
+  MX_UART4_Init();
   /* USER CODE BEGIN 2 */
 	HAL_TIM_Base_Start_IT(&htim6);
 
@@ -226,6 +243,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+		HAL_UART_Receive(&huart4, &receivedMessage, 1, HAL_MAX_DELAY);
 		HAL_UART_Receive(&huart2, &receivedMessage, 1, HAL_MAX_DELAY);
 		decodeMessage(receivedMessage);
 		if(receivedMessage == 'd'){
@@ -287,6 +305,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	if(GPIO_Pin == endstop_Pin){ // Check pin
 		isEndstopTriggered = true;
+		stopTimer();
 	}
 	else{
 		__NOP();
